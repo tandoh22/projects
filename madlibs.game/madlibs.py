3189c391e45f800dbd050a5683f310c705989239
tandoh22 <tandohsamuel4u@gmail.com>
2025-04-03 07:54:34 +0000

    new

--- madlibs.game/madlibs.py
+++ madlibs.game/madlibs.py
@@ -6,4 +6,7 @@ adjective2 = input('enter an adjective (description): ')
 verb1 = input("enter a verb ending with 'ing': ")
 adjective3 = input('enter an adjective (description): ')
 
-print(f"Today i went to an {adjective1} party")
+print(f"Today i went to a {adjective1} party")
+print(f"under the event, i saw the world's richest man, {noun1}")
+print(f"{noun1} was really {adjective2}, he couldn't stop {verb1} cash on the birthday celebrant")
+print(f"i was really {adjective3}!")
