892784c0a44f05efb4b744b1631d397b6b71bd1a
tandoh22 <tandohsamuel4u@gmail.com>
2025-04-03 07:45:34 +0000

    new

---
+++ madlibs.game/madlibs.py
@@ -0,0 +1,9 @@
+# this project is just a word game where you create a story by filling in the blanks with random words 
+
+adjective1 = input('enter an adjective (description): ')
+noun1 = input('enter a noun: ')
+adjective2 = input('enter an adjective (description): ')
+verb1 = input("enter a verb ending with 'ing': ")
+adjective3 = input('enter an adjective (description): ')
+
+print(f"Today i went to an {adjective1} party")
