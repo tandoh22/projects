74bac0ec46a21b421b5d25d52bdb7780f35af2fe
tandoh22 <tandohsamuel4u@gmail.com>
2025-04-04 13:04:01 +0000

    new

---
+++ calculator program/calc.py
@@ -0,0 +1,17 @@
+# this is a mini python calculator program
+operator = input("enter an operator: ")
+num1 = float(input('enter the first number: '))
+num2 = float(input('enter the second number: '))
+
+if operator == '+':
+    result = num1 + num2
+    print(result)
+elif operator == '-':
+    result = num1 - num2
+    print(result)    
+elif operator == '*':
+    result = num1 * num2
+    print(result)    
+elif operator == '/':
+    result = num1 / num2
+    print(result)      
