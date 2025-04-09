8a12c1ad55133a3b77564a170c37f46fe2dc87a7
tandoh22 <tandohsamuel4u@gmail.com>
2025-04-09 22:39:39 +0000

    new

--- interest calculator/int.py
+++ interest calculator/int.py
@@ -6,3 +6,11 @@ while principal <= 0:
     principal = float(input('enter the principal amount: '))
     if principal <= 0:
         print('principal amount can not be less than or equal to zero')
+while rate <= 0:
+    rate = float(input('enter the rate: '))
+    if rate <= 0:
+        print('rate can not be less than or equal to zero')
+while time <= 0:
+    time = float(input('enter the time in years: '))
+    if time <= 0:
+        print('time can not be less than or equal to zero')
