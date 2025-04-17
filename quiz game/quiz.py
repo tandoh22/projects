00d7ec9f492d0acc74da400546f4e9d873791361
tandoh22 <tandohsamuel4u@gmail.com>
2025-04-17 13:18:12 +0000

    new

--- quiz game/quiz.py
+++ quiz game/quiz.py
@@ -7,13 +7,17 @@ questions = (
     "The brain of the computer system is called____? "
 )
 
-options = (("A. B. C. D."), 
-           ("A. B. C. D."), 
-           (), 
-           (), 
-           ()
+options = (("A. 1958", "B. 1800", "C. 1957", "D. 1902"), 
+           ("A. Hard disk drive", "B. floppy disk", "C. RAM", "D. ROM"), 
+           ("A. Windows", "B. macOS", "C. Linux", "D. Chrome OS"), 
+           ("A. Optical session", "B. Operating Systems", "C. Operating section", "D. Output system"), 
+           ("A. RAM", "B. ROM", "C. Hard drive", "D. CPU")
 )
-answers = ()
+answers = ("C", "A", "C", "B", "D")
 guesses = []
 score = 0
 question_num = 0
+
+for question in questions:
+    print("------------------------------------")
+    print(question)
