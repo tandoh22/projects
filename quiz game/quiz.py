8ef5c5c142946b0888ae07fa0f45dc36915c16df
tandoh22 <tandohsamuel4u@gmail.com>
2025-04-17 13:07:04 +0000

    NEW

--- quiz game/quiz.py
+++ quiz game/quiz.py
@@ -1,6 +1,19 @@
 # python quiz game
-questions = ()
+questions = (
+    "What year did Ghana gain their independence? "
+    "What is the main memory of the computer? "
+    "Which of the following operating systems is open-source? "
+    "What does OS stands for? "
+    "The brain of the computer system is called____? "
+)
 
-options = ((), (), (), (), ())
+options = (("A. B. C. D."), 
+           ("A. B. C. D."), 
+           (), 
+           (), 
+           ()
+)
 answers = ()
 guesses = []
+score = 0
+question_num = 0
