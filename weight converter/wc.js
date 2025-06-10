4ffe9de6851bf586922ef6f207e45169b5009938
tandoh22 <tandohsamuel4u@gmail.com>
2025-06-10 16:06:45 +0000

    new

--- weight converter/wc.js
+++ weight converter/wc.js
@@ -4,9 +4,14 @@ let lbsRef = document.getElementById("lbs");
 let convertFromKg = () => {
     let kg = kgRef.value;
     lbsRef.value = (kg * 2.205).toFixed(2);
-}
+};
 
 let convertFromLbs = () => {
-    
+    let lbs = lbsRef.value;
     kgRefRef.value = (lbs / 2.205).toFixed(2);
-}
+};
+
+kgRef.addEventListener("input", convertFromKg);
+lbsRef.addEventListener("input", convertFromLbs);
+
+window.addEventListener("load", convertFromKg);
